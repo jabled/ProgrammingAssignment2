b9b08ab63b70dc69f9623d075b6ccaf908497960
jabled <jeremyabled@gmail.com>
2015-04-26 13:56:47 -0700

    Update cachematrix.R
    Added new code for completion of assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## Create the makeCacheMatrix matrix function, which creates a set
+## of stepped functions for the cacheSolve function to use
+## functions create matrix, get matrix value, invert and store value,
+## then get inverted value from cache and return value to working environment
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function (x=matrix()){
+cache<-NULL
 
+set<-function(y){
+x<<-y
+cache<<-NULL}
+
+get<-function()x
+setMatrix<-function(inverse) cache<<-inverse
+getInverse<-function()cache
+
+list(set=set,get=get,setMatrix=setMatrix,getInverse=getInverse)
 }
 
+##cacheSolve function depends on the makeCacheMatrix function
+## cacheSolve tests to see if cached data is present and then
+## creates Matrix, runs through error checking of matrix, then
+## inverts, stores and returns matrix
 
-## Write a short comment describing this function
+cacheSolve<-function(x,...){
+cache<-x$getInverse()
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+if(!is.null(cache)){
+message("retrieving cached data if present")
+return(cache)}
+
+matrix<-x$get()
+
+tryCatch(
+{cache<-solve(matrix,...)},
+error=function(error_output)
+{message("error:")
+message(error_output)
+return(NA)},
+alert=function(error_output)
+{message("warning:")
+message(error_output)
+return(NA)},
+results={x$setMatrix(cache)}
+)
+return(cache)
 }
+
