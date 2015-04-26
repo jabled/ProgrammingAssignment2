a48bdbc2f2a1d337561fcdefa2d073dfc970084f
jabled <jeremyabled@gmail.com>
2015-04-26 16:26:08 -0700

    Update cachematrix.R
    added more comments, as requested

--- cachematrix.R
+++ cachematrix.R
@@ -4,17 +4,23 @@
 ## functions create matrix, get matrix value, invert and store value,
 ## then get inverted value from cache and return value to working environment
 
+# create the cache matrix function
 makeCacheMatrix <- function (x=matrix()){
+
+# create the cache function and initialize it (to null)
 data_cache<-NULL
 
+# create the function to set the matrix
 set<-function(y){
 x<<-y
 data_cache<<-NULL}
 
+# create the additional functions to grab the matrix, invert it and then store it
 get<-function()x
 setMat<-function(inverse)data_cache<<-inverse
 getInv<-function()data_cache
 
+# create a list with all of the matrix values
 list(set=set,get=get,setMat=setMat,getInv=getInv)
 }
 
@@ -26,14 +32,19 @@ list(set=set,get=get,setMat=setMat,getInv=getInv)
 cacheSolve<-function(x,...){
 data_cache<-x$getInv()
 
+# check to see if the cache contains data or not
+# assumption is that the cache will contain data per assignment instructions so error checking is not necessary
+
 if(!is.null(data_cache)){
 message("retrieving cached data if present")
 return(data_cache)}
 
 final_matrix<-x$get()
 
+#employ the solve function to create the final inverse matrix
 data_cache<-solve(final_matrix,...)
 
+#set/store and return the value of the cached inverted matrix
 x$setMat(data_cache)
 
 return(data_cache)
